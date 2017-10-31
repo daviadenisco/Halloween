648d4a8955b2b2281ef6435f3526f9c3ff7aa68b
Davia DeNisco <davia622@gmail.com>
2017-10-31 11:55:38 -0400

    Add array of commands

--- halloween.js
+++ halloween.js
@@ -1,2 +1,3 @@
 var faveCandy = ["reece's peanut butter cups", "raisinets", "hershey's almond bar", "sour patch kids", "m & m's"];
 var faveCostumes = ["cat", "spider", "witch"];
+var commands =[ 'mkdir getGit', 'ls', 'getGit', 'git init', 'touch halloween.js', 'atom . halloween.js', 'git status', 'git add halloween.js', 'git status', 'git commit -m "Add candy"', 'git remote add origin https://github.com/daviadenisco/Halloween.git', 'git push -u origin master', 'git remote -v', 'atom . halloween.js', 'git status', 'git add halloween.js', 'git status', 'git commit -m "Add costumes"', 'git push'];
